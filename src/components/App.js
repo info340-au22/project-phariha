c70257356da3fcce8a5a577cc9dca026b640c6eb
Malena Arduino <malenaarduino@Malenas-MacBook-Pro.local>
2022-11-16 16:47:18 -0800

    starting with react

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function App(props) {
+
+    const navList = ['homepage', 'major', 'post'];
+    
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
