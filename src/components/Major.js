53a07247202a804993b8362eff39c9c5e1006ca5
phariha <97258699+phariha@users.noreply.github.com>
2022-11-23 19:13:47 -0800

    Update Major.js
    major page

--- src/components/Major.js
+++ src/components/Major.js
@@ -4,6 +4,8 @@ import data from '../data/data.json'
 import { useState } from 'react';
 import { List } from './List';
 import { NavBar } from './NavBar';
+import { Footer } from './Footer';
+
 
 export function Major(props) {
     const [inputText, setInputText] = useState("");
@@ -24,28 +26,26 @@ export function Major(props) {
                         </div>
                     </div>
             </header>
-            {/* <div className="row m-0">
-                <div className="card border-0" >
-                    <div className="card-body col-xs-4 col-sm-4 col-md-6">
-                    </div>
-                </div> */}
                 <div className="col-xs-4 col-sm-4 col-md-6">
                     <div className='form-edit'>
-                        <form id="form-control" className="p-2">
+                        <form id="form-control" className="p-4">
                             <input type="search bar" id="query" placeholder="Look up..." size="55" onChange={inputHandler}></input>
                             <button>Search</button>
                         </form>
                     </div>
                 </div>
-            {/* </div> */}
             <div className="container">
                 <div className="row">
-                    <h5 className="p-4">
+                    <h5 className="p-2">
                         List of possible majors:
                     </h5>
-                    <List input={inputText} />
+                    <div className='d-flex flex-wrap'>
+                        <List input={inputText} />
+                    </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     )
+}
 }
