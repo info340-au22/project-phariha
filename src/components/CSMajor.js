d1ff40e6180694538b05377a3cdf1e1e8e7e4909
elishira <eshira@uw.edu>
2022-12-09 23:39:42 -0800

    fixed major pages

--- src/components/CSMajor.js
+++ src/components/CSMajor.js
@@ -1,88 +1,62 @@
 import React from 'react';
 import { NavBar } from './NavBar';
 import { Footer } from './Footer';
+import MAJOR_DATA from '../data/major-data.json'
+import { useParams } from 'react-router-dom';
+import _ from 'lodash';
 import '../index.css';
-
-const pageName = "A Guide To the University of Washington's Major System";
-const majorName = "Computer Science";
-const prereqClasses = "Prerequisite Classes";
-
-const PREREQ_INPUT = [
-    <div><a href="http://www.washington.edu/students/crscat/math.html#math124">MATH 124, 125 & 126</a>, (or Honors <a href="http://www.washington.edu/students/crscat/math.html#math134">MATH 134, 135 & 136</a>)</div>,
-    <div><a href="https://www.washington.edu/students/crscat/cse.html#cse121">CSE 121, 122 & 123</a> or <a href="http://www.washington.edu/students/crscat/cse.html#cse142">CSE 142 & 143</a></div>,
-    <div>Five credits from the list of approved natural science courses for Computer Science on the <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses/cs-natural-science">Allen School website</a></div>,
-    <div>Five credits of <a href="https://www.washington.edu/uaa/advising/degree-overview/general-education/english-composition/">English Composition</a></div>
-];
-
-const howToApply = "How to Apply";
-
-const APP_DEADLINE_INPUT = [{
-    quarter: "autumn",
-    date: "July 1st"
-},
-{
-    quarter: "spring",
-    date: "January 15th"
-}];
-const deadlineArray = APP_DEADLINE_INPUT.map((deadline, index) => {
-    const listType = "sub-li";
-    const element = <li key={index} className={listType}>To enter the program in the {deadline.quarter} quarter: {deadline.date}</li>;
-    return element;
-});
-
-const HOW_TO_APPLY_INPUT = [
-    <div>Application Deadlines:<ul>{deadlineArray}</ul></div>, <div>3 required and 1 optional <a href="https://www.engr.washington.edu/uapp/essay_questions.phtml#dept_essay">essay questions</a></div>,
-    "Complete all prerequisites before applying",
-    <div>More information about applying to the Computer Science major can be found <a href="https://www.cs.washington.edu/academics/ugrad/admissions/currentuw">here</a></div>,
-    <div>Related majors can be found <a href="https://www.cs.washington.edu/academics/ugrad/nonmajor-options/related-majors">here</a></div>
-];
-
-const graduationReqHeadding = "Graduation Requirements: Computer Science Component";
-const fundamentals = "Fundamentals (24-25 credits)";
-const coreAndElectives = "Core and Electives (33 credits)";
-
-const coreAndElectivesText = "At least 33 additional credits from courses on the approved CSE Electives list, including at least:";
-const CORE_ELECT_INPUT = [
-    <div>(12-16 credits) Four, 400-level courses from the <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses">CSE Core Courses</a></div>,
-    <div>(6-8 credits) Two additional <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses">CSE Core Courses</a> (300 or 400 level)</div>,
-    <div>(3-5 credits)Either 1 additional <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses">CSE Core Course</a> or 1 course from the <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses/capstone-courses">CSE Capstone list</a></div>,
-    <div>(4-12 credits) Additional courses from either the <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses">CSE Core Courses</a> list or the CSE Elective list to bring total <a href="https://www.cs.washington.edu/academics/ugrad/current-students/degree/courses/elective">CSE Elective credits to 33</a></div>
-];
-
-const coreElectArray = CORE_ELECT_INPUT.map((classes, index) => {
-    let listType = "sub-li";
-    const element = <li key={index} className={listType}>{classes}</li>;
-    return element;
-})
-
-const FUNDAMENTAL_CLASSES_INPUT = [
-    "123 Intro to Computer Programming III OR (5 credits) CSE 143 Computer Programming II",
-    "311 Foundations of Computing I",
-    "312 Foundations of Computing II",
-    "331 Software Design and Implementation",
-    "332 Data Structures and Parallelism",
-    "351 The Hardware/Software Interface"
-];
-
-const prereqClassArray = PREREQ_INPUT.map((prereq, index) => {
-    const element = <li key={index}>{prereq}</li>;
-    return element;
-})
-
-const howToApplyArray = HOW_TO_APPLY_INPUT.map((step, index) => {
-    const element = <li key={index}>{step}</li>;
-    return element;
-})
-const fundamentalsArray = FUNDAMENTAL_CLASSES_INPUT.map((className, index) => {
-    let listType = "sub-li";
-    const element = <li key={index} className={listType}>(4 credits) CSE {className}</li>;
-    return element;
-})
+import { type } from '@testing-library/user-event/dist/type';
 
 export function CSMajor(props) {
+    const pageName = "A Guide To the University of Washington's Major System";
+    const prereqClasses = "Prerequisite Classes";
+    const howToApply = "How to Apply";
+    const graduationReqHeadding = "Graduation Requirements: Computer Science Component";
+    const fundamentals = "Fundamentals (24-25 credits)";
+    const coreAndElectives = "Core and Electives (33 credits)";
+
+    const majorName = "info";
+    const majorData = _.find(MAJOR_DATA.majors, { major: majorName });
+    console.log(majorData);
+    console.log(majorData.prereqCSE);
+
+    const prereqClassArray = [];
+    const howToApplyArray = [];
+    const fundamentalsArray = [];
+    const coreElectArray = [];
+    Object.keys(majorData).forEach((entry) => {
+        if (entry.startsWith("prereq")) {
+            let element = <li key={entry}>{majorData[entry]}</li>;
+            prereqClassArray.push(element);
+        };
+
+        if (entry.startsWith("required")) {
+            let element = <li key={entry}>{majorData[entry]}</li>;
+            howToApplyArray.push(element);
+        };
+
+        if (entry.startsWith("fundamental")) {
+            let element = <li key={entry}>{majorData[entry]}</li>;
+            fundamentalsArray.push(element);
+        };
+
+        if (entry.startsWith("fundamental")) {
+            let element = <li key={entry}>{majorData[entry]}</li>;
+            coreElectArray.push(element);
+        };
+
+        if (entry.startsWith("core")) {
+            let element = <li key={entry}>{majorData[entry]}</li>;
+            coreElectArray.push(element);
+        };
+    });
+
+    howToApplyArray.push(<li key={"more info "}>More information about applying to the {majorData.name} major can be found <a href={majorData.link}>here</a></li>)
+
+
     return (
         <div>
-            {/* <NavBar /> */}
+
             <div>
                 <header className="major-page">
                     <div className="container">
@@ -91,25 +65,26 @@ export function CSMajor(props) {
                         </div>
                     </div>
                 </header>
-                <h1 className="interact-h1">{majorName}</h1>
+                <h1 className="interact-h1">{majorData.name}</h1>
                 <div id="container">
                     <h2 className="interact-h2">{prereqClasses}</h2>
                     <ul className="interact-list-cs">{prereqClassArray}</ul>
 
                     <h2 className="interact-h2">{howToApply}</h2>
-                    <ul className="interact-list-cs">{howToApplyArray}</ul>
+                     <ul className="interact-list-cs">{howToApplyArray}</ul>
 
                     <h2 className="interact-h2">{graduationReqHeadding}</h2>
+                    
                     <ul className="interact-list-cs">
-                        <li className="category">{fundamentals}</li>
+                        <li className="category">{majorData.textFundamentals}</li>
                         <ul>{fundamentalsArray}</ul>
-                        <li className="category">{coreAndElectives}</li>
-                        <div>{coreAndElectivesText}</div>
+                        <li className="category">{majorData.textCore}</li>
                         <ul>{coreElectArray}</ul>
                     </ul>
+                    <div className="interact-list-cs">{majorData.bottomText}</div>
                 </div>
             </div>
-            {/* <Footer /> */}
+
         </div>
 
     )
